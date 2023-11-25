e63f03c963bf9e51bc7412c8524e071492f1352b
Sumi Seo <sumseo@paul-f4Br6s8.clusters.42paris.fr>
2023-11-25 17:52:08 +0100

    feature/add-read-function-buffer-link

--- get_next_line.c
+++ get_next_line.c
@@ -12,14 +12,17 @@
 
 #include "get_next_line.h"
 
+#define BUFFER_SIZE 5
+
 char	*get_next_line(int fd)
 {
-	char	buffer[1024];
+	char	buffer[BUFFER_SIZE];
 	int		bytesRead;
 
 	printf("fd : %d", fd);
-	bytesRead = read(fd, buffer, sizeof(buffer));
+	bytesRead = read(fd, buffer, BUFFER_SIZE);
 	printf("File contents : %d\n", bytesRead);
+	printf("File contents %s\n", buffer);
 	return ("test");
 }
 
@@ -27,7 +30,7 @@ int	main(void)
 {
 	int	fh;
 
-	fh = open("poem.txt", O_RDWR);
+	fh = open("poem2.txt", O_RDWR);
 	printf("fh[ %d]", fh);
 	get_next_line(fh);
 	return (0);

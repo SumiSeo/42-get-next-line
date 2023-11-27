cebe8760a77dfd1d9a836e51c4e41be2c64694a6
Sumi Seo <sumseo@paul-f4Br6s8.clusters.42paris.fr>
2023-11-27 17:34:12 +0100

    feature/gnl-function

--- get_next_line.h
+++ get_next_line.h
@@ -13,13 +13,31 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+typedef struct s_list
+{
+	char			*str_buf;
+	struct s_list	*next;
+}					t_list;
+
 # include <fcntl.h>
 # include <stddef.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
-char	*get_next_line(int fd);
-size_t	get_next_line_utils(int fd, int size);
+char				*get_next_line(int fd);
+int					len_to_newline(t_list *list);
+void				dealloc(t_list **list, t_list *clean_node, char *buf);
+void				copy_str(t_list *list, char *str);
+t_list				*find_last_node(t_list *list);
+int					found_newline(t_list *list);
+char				*get_line(t_list *list);
+void				append(t_list **list, char *buf);
+void				create_list(t_list **list, int fd);
+char				*get_next_line(int fd);
 
 #endif
